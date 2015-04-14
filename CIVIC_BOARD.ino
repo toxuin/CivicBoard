a2b516054264e0af4ce9f6bb8ba5e6912e112a05
toxuin <otstoypuksa@ya.ru>
2015-04-13 18:41:10 -0700

    LED test

--- CIVIC_BOARD.ino
+++ CIVIC_BOARD.ino
@@ -1,17 +1,96 @@
-//The setup function is called once at startup of the sketch
-void setup()
-{
-// Add your initialization code here
-	pinMode(13, OUTPUT);
-	
+#include <Servo.h>
+#include <SoftwareSerial.h>
+
+Servo esc;
+SoftwareSerial bluetooth(3, 2); // RX, TX
+
+int ledPin = 13;
+int escPin = 9;
+int upBtnPin = 5;
+int centerBtnPin = 6;
+int downBtnPin = 7;
+
+const int UP = 180;
+const int DOWN = 20;
+const int CENTER = 120;
+
+unsigned int pos = DOWN;
+
+// BLUETOOTH STUFF
+String Data = "";
+
+void setup() {
+  esc.attach(escPin);
+  
+  Serial.begin(9600);
+  bluetooth.begin(9600);
+  delay(500);
+  
+  pinMode(ledPin, OUTPUT);
+  pinMode(upBtnPin, INPUT);
+  pinMode(centerBtnPin, INPUT);
+  pinMode(downBtnPin, INPUT);
+  
+  Serial.println("READY!");
+  
+  //esc.write(0);
+  //delay(2000);
+} 
+
+
+void loop() { 
+  
+  // BUTTONS FOR ESC
+  if (digitalRead(downBtnPin) == HIGH) {
+    // pos = DOWN;
+    pos--;
+  } else if (digitalRead(centerBtnPin) == HIGH) {
+    // pos = CENTER;
+    pos = DOWN;
+  } else if (digitalRead(upBtnPin) == HIGH) {
+    // pos == UP;
+    pos++;
+  }
+  //outputVar("position:", pos);
+  
+  // ERROR CONTROL
+  if (pos > 180) pos = 180;
+  else if (pos < 10) pos = 10;
+
+  startListenForBT();
+
+  // COMMIT 
+  //esc.write(pos);
+  //delay(200);
+  
+  
+  
 }
 
-// The loop function is called in an endless loop
-void loop()
-{
-//Add your repeated code here
-	digitalWrite(13, HIGH);
-	sleep(100);
-	digitalWrite(13, LOW);
-	sleep(100);
+void startListenForBT() {
+  while (bluetooth.available()) {
+    Serial.println("BLUETOOTH OK");
+    char character = bluetooth.read();
+    Data = Data + character;
+    if (character == 59) { // ;
+      Serial.print("Received: ");
+      Serial.println(Data);
+      
+      
+      if (Data == "ON;") {
+        digitalWrite(ledPin, HIGH);
+        Serial.print("LED ON");
+      } else if (Data == "OFF;") {
+        digitalWrite(ledPin, LOW);
+        Serial.print("LED OFF");
+      } else {
+        Serial.print("NOT A COMMAND! LENGTH: ");
+        Serial.println(Data.length());
+      }
+      
+      
+      Data = "";
+    }
+  }
 }
+
